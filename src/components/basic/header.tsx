75d80cdb54c7cc306bcbbcb7eced1bb6eb037dd8
Viktor <ogivita11q@gmail.com>
2024-09-28 16:16:05 +0300

    feat: fix

--- src/components/basic/header.tsx
+++ src/components/basic/header.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 interface HeaderProps {
-  onToggleSidebar: () => void;
+  onToggleSidebar?: () => void;
 }
 
 export default function Header({ onToggleSidebar }: HeaderProps) {
